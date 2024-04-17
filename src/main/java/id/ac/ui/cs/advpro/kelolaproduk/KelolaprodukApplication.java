9c080ba5650398b3ee02bc73c970f196704527ac
aldenluthfi <alden.luthfi@ui.ac.id>
2024-04-17 19:27:08 +0700

    First Commit!

---
+++ src/main/java/id/ac/ui/cs/advpro/kelolaproduk/KelolaprodukApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advpro.kelolaproduk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelolaprodukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelolaprodukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advpro/kelolaproduk/KelolaprodukApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advpro.kelolaproduk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelolaprodukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

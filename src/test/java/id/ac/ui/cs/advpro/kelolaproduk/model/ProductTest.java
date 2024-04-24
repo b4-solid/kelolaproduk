2567ebe6ba4275653c03449ae1b1a3c184d61bac
RaquelNayyara <125016793+RaquelNayyara@users.noreply.github.com>
2024-04-24 17:45:45 +0700

    [RED] Make test for product model

---
+++ src/test/java/id/ac/ui/cs/advpro/kelolaproduk/model/ProductTest.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advpro.kelolaproduk.model;
+
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+    Product product;
+    @Test
+    void testGetUserID() {
+
+    }
+
+    @Test
+    void testGetTitle() {
+
+    }
+
+    @Test
+    void testGetDescription() {
+
+    }
+
+    @Test
+    void testGetStock() {
+
+    }
+
+    @Test
+    void testGetImageUrl() {
+
+    }
+}

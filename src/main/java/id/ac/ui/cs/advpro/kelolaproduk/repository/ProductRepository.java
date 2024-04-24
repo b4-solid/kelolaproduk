1c30ab3cb3b19267fd56ec11f2af7142c80fff42
RaquelNayyara <125016793+RaquelNayyara@users.noreply.github.com>
2024-04-24 18:07:20 +0700

    [GREEN] Implementation product repository

--- src/main/java/id/ac/ui/cs/advpro/kelolaproduk/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advpro/kelolaproduk/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package id.ac.ui.cs.advpro.kelolaproduk.repository;
 
+import id.ac.ui.cs.advpro.kelolaproduk.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ProductRepository {
+import java.util.List;
+import java.util.UUID;
 
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    List<Product> findByIsDiscountedTrue();
 }

c22d5a751df3897481407dd3f6d159d47bb8dfb9
RaquelNayyara <125016793+RaquelNayyara@users.noreply.github.com>
2024-04-24 17:57:47 +0700

    [GREEN] Implementation interface product service

--- src/main/java/id/ac/ui/cs/advpro/kelolaproduk/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advpro/kelolaproduk/service/ProductService.java
@@ -1,5 +1,14 @@
 package id.ac.ui.cs.advpro.kelolaproduk.service;
 
+import id.ac.ui.cs.advpro.kelolaproduk.model.Product;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public interface ProductService {
-   
+    Product getProductById(UUID id);
+    Product addProduct(Product product);
+    Product updateProduct(Product product);
+    void deleteProduct(UUID productId);
+    void applyDiscount(UUID productId, BigDecimal discountRate);
 }

6e4baa7aa45923731491da434f8fef98b2c7f38a
aldenluthfi <aldenluthfi@gmail.com>
2024-05-24 11:42:03 +0700

    [REFACTOR] Changed model name

---
+++ src/test/java/id/ac/ui/cs/advpro/kelolaproduk/controller/ProductModelControllerTest.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advpro.kelolaproduk.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(ProductController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class ProductControllerTest {
+    // TODO: Implement test for ProductController
+}
